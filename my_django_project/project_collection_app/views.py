06a3f95d0121fc208f3f4220acbf7b80d5619f29
laurivirtanen <lauri.virtanen@elisa.fi>
2022-03-28 16:11:21 +0300

    fixed exception cases with get,put,delete operations when id is not found

--- my_django_project/project_collection_app/views.py
+++ my_django_project/project_collection_app/views.py
@@ -9,6 +9,8 @@ class ProjectViews(APIView):
     def post(self, request):
         serializer = GithubProjectSerializer(data=request.data)
         if serializer.is_valid():
+            print("FOOBARRRRRR")
+            print(self.request.user)
             serializer.save()
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
         else:
@@ -16,42 +18,48 @@ class ProjectViews(APIView):
 
     def get(self, request, id=None):
         if id:
-            item = GithubProject.objects.get(id=id)
-            serializer = GithubProjectSerializer(item)
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
-        
+            try:
+                item = GithubProject.objects.get(id=id)
+                serializer = GithubProjectSerializer(item)
+                return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+            except:
+                return Response(
+                {"res": "Object with id does not exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
         items = GithubProject.objects.all()
         serializer = GithubProjectSerializer(items, many=True)
         return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
 
     def put(self, request, id):
-        item = GithubProject.objects.get(id=id)
-        if not item:
+        try:
+            item = GithubProject.objects.get(id=id)
+            data = {
+                'name': request.data.get('name'), 
+                'description': request.data.get('description'), 
+                'link': request.data.get('link'),
+                'rating': request.data.get('rating'),
+            }
+            serializer = GithubProjectSerializer(instance = item, data=data, partial = True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
             return Response(
                 {"res": "Object with id does not exists"}, 
                 status=status.HTTP_400_BAD_REQUEST
             )
-        data = {
-            'name': request.data.get('name'), 
-            'description': request.data.get('description'), 
-            'link': request.data.get('link'),
-            'rating': request.data.get('rating'),
-        }
-        serializer = GithubProjectSerializer(instance = item, data=data, partial = True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id):
-        item = GithubProject.objects.get(id=id)
-        if not item:
+        try:
+            item = GithubProject.objects.get(id=id)
+            item.delete()
+            return Response(
+                {"res": "Object deleted!"},
+                status=status.HTTP_200_OK
+            )
+        except:
             return Response(
                 {"res": "Object with id does not exists"}, 
                 status=status.HTTP_400_BAD_REQUEST
             )
-        item.delete()
-        return Response(
-            {"res": "Object deleted!"},
-            status=status.HTTP_200_OK
-        )
