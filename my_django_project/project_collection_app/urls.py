0cba1eface7dff35809bd0a736d0846551a10aa7
laurivirtanen <lauri.virtanen@elisa.fi>
2022-03-29 22:10:43 +0300

    changed views to implement List (ListCreateAPIView) and Detail (RetrieveUpdateDestroyAPIView) classes, this supports object level permission checks better than previous solution. changed also urls accordingly, permissions.py added as a new module

--- my_django_project/project_collection_app/urls.py
+++ my_django_project/project_collection_app/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import ProjectViews
+#from .views import ProjectViews
+from .views import GithubProjectList
+from .views import GithubProjectDetail
+
 
 urlpatterns = [
-    path('github_projects/', ProjectViews.as_view()),
-    path('github_projects/<int:id>', ProjectViews.as_view())
+    path('github_projects/', GithubProjectList.as_view()),
+    path('github_projects/<int:pk>', GithubProjectDetail.as_view())
 ]
+
+#urlpatterns = [
+    #path('github_projects/', ProjectViews.as_view()),
+    #path('github_projects/<int:id>', ProjectViews.as_view())
+#]
--- my_django_project/project_collection_app/views.py
+++ my_django_project/project_collection_app/views.py
@@ -1,68 +1,20 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 from .models import GithubProject
 from .serializers import GithubProjectSerializer
+from rest_framework import permissions
+from project_collection_app.permissions import IsOwnerOrReadOnly
 
-class ProjectViews(APIView):
+class GithubProjectList(generics.ListCreateAPIView):
+    queryset = GithubProject.objects.all()
+    serializer_class = GithubProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-    permission_classes = (IsAuthenticated,)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-    def post(self, request):
-        serializer = GithubProjectSerializer(data=request.data)
-        if serializer.is_valid():
-            print("FOOBARRRRRR")
-            print(self.request.user)
-            serializer.save()
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
-        else:
-            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+class GithubProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = GithubProject.objects.all()
+    serializer_class = GithubProjectSerializer
+    permission_classes = [permissions.IsAuthenticated,
+                          IsOwnerOrReadOnly]
 
-    def get(self, request, id=None):
-        if id:
-            try:
-                item = GithubProject.objects.get(id=id)
-                serializer = GithubProjectSerializer(item)
-                return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
-            except:
-                return Response(
-                {"res": "Object with id does not exists"}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        items = GithubProject.objects.all()
-        serializer = GithubProjectSerializer(items, many=True)
-        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
-
-    def put(self, request, id):
-        try:
-            item = GithubProject.objects.get(id=id)
-            data = {
-                'name': request.data.get('name'), 
-                'description': request.data.get('description'), 
-                'link': request.data.get('link'),
-                'rating': request.data.get('rating'),
-            }
-            serializer = GithubProjectSerializer(instance = item, data=data, partial = True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-        except:
-            return Response(
-                {"res": "Object with id does not exists"}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-    def delete(self, request, id):
-        try:
-            item = GithubProject.objects.get(id=id)
-            item.delete()
-            return Response(
-                {"res": "Object deleted!"},
-                status=status.HTTP_200_OK
-            )
-        except:
-            return Response(
-                {"res": "Object with id does not exists"}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
