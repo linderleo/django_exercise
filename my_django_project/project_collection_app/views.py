da769319423386997e369235cda735d964ac1683
laurivirtanen <lauri.virtanen@elisa.fi>
2022-04-02 17:27:34 +0300

    webhooks are now called outside request response cycle, asynchronously. removed from requests module to httpx, asyncio

--- my_django_project/project_collection_app/views.py
+++ my_django_project/project_collection_app/views.py
@@ -3,33 +3,26 @@ from .models import GithubProject, Webhook
 from .serializers import GithubProjectSerializer, WebhookSerializer
 from rest_framework import permissions
 from project_collection_app.permissions import IsOwnerOrReadOnly
-import requests
 import json
+import httpx
+import asyncio
 
 class GithubProjectList(generics.ListCreateAPIView):
     queryset = GithubProject.objects.all()
     serializer_class = GithubProjectSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    # Below we do asynchronous webhook calls when we add a new project entry
     def perform_create(self, serializer):
-        def webhook_call(payload, url: str):
-            try:
-                print(f"LÄHTEE KUTSU: {url}")
-                r = requests.post(url, data=json.dumps(payload), headers={'Content-Type': 'application/json'})
-                print(r)
-                r.raise_for_status()
-            except requests.exceptions.HTTPError as errh:
-                print(f"Http Error: {errh}")
-            except requests.exceptions.ConnectionError as errc:
-                print(f"Error Connecting: {errc}")
-            except requests.exceptions.Timeout as errt:
-                print(f"Timeout Error: {errt}")
-            except requests.exceptions.RequestException as err:
-                print(f"Oops: Something else happened: {err}")
+        async def main(data, urls):
+            async with httpx.AsyncClient() as client:
+                for hook in urls:
+                    print(f"Calling webhook: {hook}")
+                    r = await client.post(hook, json=data)
 
         serializer.save(owner=self.request.user)
         hooks = Webhook.objects.values_list('url')
-        [webhook_call(serializer.data, hook[0]) for hook in hooks]
+        asyncio.run(main(serializer.data, urls = [hook[0] for hook in hooks]))
 
 class GithubProjectDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = GithubProject.objects.all()
